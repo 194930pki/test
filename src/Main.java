23dfe0d073c7751c319a7904a1d61859c73b3884
194930 <194930@edu.p.lodz.pl>
2019-03-08 09:09:03 +0100

    Added "add" method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String args[]) {
+        System.out.println(add(2, 3));
+    }
 
+    private static int add(int a, int b){
+        return a + b;
     }
 }
