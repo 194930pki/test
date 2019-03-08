6c9b08932ea17dfa33541c34afe8dc7617350469
194930 <194930@edu.p.lodz.pl>
2019-03-08 09:18:34 +0100

    Added "multiply" method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println(add(2, 3));
+        System.out.println(multiply(2, 3));
     }
 
     private static int add(int a, int b){
         return a + b;
+    }
+
+    private static int multiply(int a, int b){
+        return a * b;
     }
 }
